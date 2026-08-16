82ded0bb7ea8806582d22574df644b31116857e9
SweZinAung2393 <swezinaung.23.cu@gmail.com>
2026-08-16 10:42:43 +0630

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,3 @@ for i in range(1, 20):
                     st.markdown(res.choices[0].message.content)
             else:
                 st.warning("ကျေးဇူးပြု၍ စာသားများ ထည့်သွင်းပါ။")
-                
